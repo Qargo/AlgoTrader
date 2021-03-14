b6c54521aeb41ba4ac733502351de9fb3155e33f
serdjo64 <serdjo@yahoo.com>
2021-03-14 13:12:20 -0400

    finish download data related changes

--- download_data.py
+++ download_data.py
@@ -4,6 +4,8 @@
 Created on Sun Mar 14 08:49:06 2021
 
 @author: srdjan
+downloads financial data for all stocks specified in config file given start
+and end date. Save the downloaded data to csv file.
 """
 import os
 import sys
@@ -31,15 +33,46 @@ def build_parser():
 def main():
     parser = build_parser()
     options = parser.parse_args()
-    
+
+    # add following folders
     if not os.path.exists("./" + config.DATA_SAVE_DIR):
         os.makedirs("./" + config.DATA_SAVE_DIR)
 
-    df = YahooDownloader(start_date=config.START_DATE,
-                         end_date=config.END_DATE,
-                         ticker_list=config.DOW_30_TICKER).fetch_data()
-    df.to_csv("./" + config.DATA_SAVE_DIR + "/" + options.name + ".csv")
+    # From config.py file get following:
+    # start_date
+    START_DATE = config.START_DATE
 
-        
+    # end_date
+    END_DATE = config.END_DATE
+
+    # list of stocks#
+    STOCK_LIST = config.DOW_30_TICKER
+
+    print("All stocks used for training:", STOCK_LIST)
+    print("Historical data are used from: ", START_DATE)
+    print("Till end date: ", END_DATE)
+
+    # Download and save the data in a pandas DataFrame:
+    data_frame = YahooDownloader(start_date = START_DATE,
+                                 end_date = END_DATE,
+                                 ticker_list = STOCK_LIST).fetch_data()
+
+    print("Data Frame shape is: ", data_frame.shape)
+    print("Data Frame format is following: \n\n", data_frame.head())
+    
+    ##
+    ## Save downloaded data to file
+    ##
+    if os.path.exists("./" + config.DATA_SAVE_DIR + "/" + options.name + ".csv"):
+        os.remove("./" + config.DATA_SAVE_DIR + "/" + options.name + ".csv")
+        print("The download data file deleted")
+    else:
+        print("The download data file does not exist")
+
+    data_frame.to_csv("./" + config.DATA_SAVE_DIR + "/" + options.name + ".csv")
+
+    print("Successfuly completed the task of downloading and saving financial data.")
+
+
 if __name__ == "__main__":
     main()
--- load_and_preprocess_data.py
+++ load_and_preprocess_data.py
@@ -4,7 +4,7 @@
 Created on Mon Mar  14 2021
 
 @author: dejan
-loads financial data for all stocks specified in config file and
+loads downloaded financial data for all stocks specified in config file and
 add technical indicators. Split the dataset on training and testing set.
 """
 # import all libraries
@@ -12,96 +12,98 @@ import pickle
 
 import os
 import sys
+import pandas as pd
 import warnings
+from argparse import ArgumentParser
+
 
 # add FinRL-Library path
 sys.path.append("./FinRL-Library")
 
 from algotrader.config import config
-from finrl.marketdata.yahoodownloader import YahooDownloader
 from finrl.preprocessing.preprocessors import FeatureEngineer
 from finrl.preprocessing.data import data_split
 
 
-# Basic setup
-#Disable warnings
-warnings.filterwarnings('ignore')
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--name",
+        dest="name",
+        help="",
+        metavar="NAME",
+        default="dow_30_ticker",
+    )
+    return parser
 
-# add following folders
-if not os.path.exists("./" + config.DATA_SAVE_DIR):
-    os.makedirs("./" + config.DATA_SAVE_DIR)
 
-# From config.py file get following:
-# start_date
-START_DATE = config.START_DATE
+def main():
+    parser = build_parser()
+    options = parser.parse_args()
 
-# end_date
-END_DATE = config.END_DATE
+    # Basic setup
+    #Disable warnings
+    warnings.filterwarnings('ignore')
 
-# list of stocks#
-STOCK_LIST = config.DOW_30_TICKER
+    # Load the saved data in a pandas DataFrame:
+    data_frame = pd.read_csv("./" + config.DATA_SAVE_DIR + "/" + options.name + ".csv")
 
-print("All stocks used for training:",STOCK_LIST)
-print("Historical data are used from: ", START_DATE)
-print("Till end date: ", END_DATE)
+    print("Data Frame shape is: ", data_frame.shape)
+    print("Data Frame format is following: \n\n", data_frame.head())
 
-# Download and save the data in a pandas DataFrame:
-data_frame = YahooDownloader(start_date = START_DATE,
-                             end_date = END_DATE,
-                             ticker_list = STOCK_LIST).fetch_data()
+    ## we store the stockstats technical indicator column names in config.py
+    tech_indicator_list=config.TECHNICAL_INDICATORS_LIST
+    print("Technical Indicators that are going to be calculated: ", tech_indicator_list)
 
-print("Data Frame shape is: ", data_frame.shape)
-print("Data Frame format is following: \n\n", data_frame.head())
+    feature_engineering = FeatureEngineer(
+                        use_technical_indicator=True,
+                        tech_indicator_list=tech_indicator_list,
+                        use_turbulence=True,
+                        user_defined_feature=False)
 
-## we store the stockstats technical indicator column names in config.py
-tech_indicator_list=config.TECHNICAL_INDICATORS_LIST
-print("Technical Indicators that are going to be calculated: ", tech_indicator_list)
+    processed = feature_engineering.preprocess_data(data_frame)
 
-feature_engineering = FeatureEngineer(
-                    use_technical_indicator=True,
-                    tech_indicator_list=tech_indicator_list,
-                    use_turbulence=True,
-                    user_defined_feature=False)
+    print(processed.sort_values(['date','tic'], ignore_index=True).head(10))
 
-processed = feature_engineering.preprocess_data(data_frame)
+    training_set = data_split(processed, config.START_DATE, config.START_TRADE_DATE)
+    testing_set = data_split(processed, config.START_TRADE_DATE, config.END_DATE)
+    print("Size of training set: ", len(training_set))
+    print("Size of testing set: ", len(testing_set))
 
-print(processed.sort_values(['date','tic'], ignore_index=True).head(10))
+    print("Training set format:\n\n", training_set.head())
 
-training_set = data_split(processed, config.START_DATE, config.START_TRADE_DATE)
-testing_set = data_split(processed, config.START_TRADE_DATE, config.END_DATE)
-print("Size of training set: ", len(training_set))
-print("Size of testing set: ", len(testing_set))
+    print("Testing set format: \n\n", testing_set.head())
 
-print("Training set format:\n\n", training_set.head())
+    stock_dimension = len(training_set.tic.unique())
+    state_space = 1 + 2*stock_dimension + len(tech_indicator_list)*stock_dimension
+    print(f"Stock Dimension: {stock_dimension}, State Space: {state_space}")
 
-print("Testing set format: \n\n", testing_set.head())
+    ##
+    ## Save data to file, both training and trading
+    ##
+    if os.path.exists("./" + config.DATA_SAVE_DIR + "/training.txt"):
+        os.remove("./" + config.DATA_SAVE_DIR + "/training.txt")
+        print("The training data file deleted")
+    else:
+        print("The training data file does not exist")
 
-stock_dimension = len(training_set.tic.unique())
-state_space = 1 + 2*stock_dimension + len(tech_indicator_list)*stock_dimension
-print(f"Stock Dimension: {stock_dimension}, State Space: {state_space}")
+    if os.path.exists("./" + config.DATA_SAVE_DIR + "/testing.txt"):
+        os.remove("./" + config.DATA_SAVE_DIR + "/testing.txt")
+        print("The testing data file deleted")
+    else:
+        print("The testing data file does not exist")
 
-##
-## Save data to file, both training and trading
-##
-if os.path.exists("./" + config.DATA_SAVE_DIR + "/training.txt"):
-    os.remove("./" + config.DATA_SAVE_DIR + "/training.txt")
-    print("The training data file deleted")
-else:
-    print("The training data file does not exist")
+    path_training = "./" + config.DATA_SAVE_DIR + "/training.txt"
+    path_testing = "./" + config.DATA_SAVE_DIR + "/testing.txt"
 
-if os.path.exists("./" + config.DATA_SAVE_DIR + "/testing.txt"):
-    os.remove("./" + config.DATA_SAVE_DIR + "/testing.txt")
-    print("The testing data file deleted")
-else:
-    print("The testing data file does not exist")
+    with open(path_training, "wb") as f:
+        pickle.dump(training_set, f, pickle.HIGHEST_PROTOCOL)
 
-path_training = "./" + config.DATA_SAVE_DIR + "/training.txt"
-path_testing = "./" + config.DATA_SAVE_DIR + "/testing.txt"
+    with open(path_testing, "wb") as f:
+        pickle.dump(testing_set, f, pickle.HIGHEST_PROTOCOL)
 
-with open(path_training, "wb") as f:
-    pickle.dump(training_set, f, pickle.HIGHEST_PROTOCOL)
+    print("Successfuly completed the task of creation of test and training data files.")
 
-with open(path_testing, "wb") as f:
-    pickle.dump(testing_set, f, pickle.HIGHEST_PROTOCOL)
 
-print("Successfuly completed the task of creation of test and training data files.")
+if __name__ == "__main__":
+    main()
